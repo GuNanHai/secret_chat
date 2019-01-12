9abd699ce663d32d7b7255bd8499679b0be2b63e
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2019-01-12 22:42:10 +0800

    not important

--- src/App.js
+++ src/App.js
@@ -135,10 +135,10 @@ class App extends Component{
 			$('#ChatZone').scrollTop($('#ChatZone')[0].scrollHeight);
 		}
 	}
-	//=======限制本地消息条数不超出25条===============================
+	//=======限制本地消息条数不超出150条===============================
 
 	deleteChatItemWhenOverFlow(){
-		if(this.state.chatText.length>30){
+		if(this.state.chatText.length>150){
 			let temp = this.state.chatText;
 			temp.shift();
 			this.setState({chatText:temp});
