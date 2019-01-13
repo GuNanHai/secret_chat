b1871aa8f15150472d163ab16354a6fc09d3ba5e
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2019-01-13 12:31:50 +0800

    微调信息来源显示

--- src/SpeechBubbleMe.js
+++ src/SpeechBubbleMe.js
@@ -2,20 +2,23 @@ import React from 'react';
 import './SpeechBubble.css';
 
 let bubbleStyle = '';
-
+let situation = '';
 const SpeechBubbleMe = ({address,chatItem,id,elementHeight,datetime,ipAddress}) => {
 	// console.log('SpeechBubbleMe更新');
 
 	if(address === 'local'){
 		bubbleStyle = 'odd';
+		situation = '本地：';
+
 	}else{
 		bubbleStyle = 'even';
+		situation = '来自：';
 	}
 	return (
 		  <div className={`bubble ${bubbleStyle}`}
 		  id={`chatItem${id}`} 
 		  style={{height:elementHeight+'px'}}>
-		  	<p><span className="messageSource">本地：{ipAddress}</span>{chatItem}<span className="timestamp">{datetime}</span></p>
+		  	<p><span className="messageSource">{situation}{ipAddress}</span>{chatItem}<span className="timestamp">{datetime}</span></p>
 		  </div>
 		);
 }
